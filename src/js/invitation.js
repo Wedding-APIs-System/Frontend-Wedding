adb9daa86b722a3134456cca13ea8012f7ac8fe4
Sergio Tovar <stovarm@gmail.com>
2021-09-06 22:46:47 -0500

    Complete Style css for invitation add new photos

--- src/js/invitation.js
+++ src/js/invitation.js
@@ -1,4 +1,5 @@
 const btnToggle = document.querySelector('.sidebar_tab');
 btnToggle.addEventListener('click', function() {
-    document.getElementById('sidebar').classList.toggle('active');    
+    document.getElementById('sidebar').classList.toggle('active');
+    //btnToggle.innerHTML = '<img src="https://image.flaticon.com/icons/png/512/1231/1231223.png">';    
 });
