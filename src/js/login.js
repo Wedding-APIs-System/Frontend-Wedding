2681bf5955cd2581a3b3e3feeac32aa7969aaf9c
Sergio Tovar <stovarm@gmail.com>
2021-09-14 22:34:15 -0500

    Solution for Login send to invitation complete... using onclick and validate with data.detail

--- src/js/login.js
+++ src/js/login.js
@@ -1,4 +1,4 @@
-//CODIGO 1 CON SUBMIT
+/* //CODIGO 1 CON SUBMIT
 
 const form = document.getElementById('login_form');
 let number = document.forms['form']['phone_number'];
@@ -44,26 +44,44 @@ form.addEventListener('submit',(e) =>{
         console.log(url);
         console.log("Incorrecto");
     }
-})  
+})   */
 
 
 //CODIGO 2 CON BUTTON ONCLICK
 
+function loginValidate() {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    const number = document.forms['form']['phone_number'];
+    const number_error = document.getElementById('phone_error');
+    const API = 'https://wedding-325203.rj.r.appspot.com/login/';
+    let guest = document.getElementById('phone').value;
+    let url = (`${API}${guest}`);
+    
+    if (guest.length == 10){
+        fetch(url)
+            .then(response => response.json())                  
+            .then(data => {
+                if (data.detail == 'User not found'){
+                   number.style.border = "2px solid red";
+                   number_error.style.display = "block";
+                   number.focus();
+                   number_error.innerText = "Usuario No existe"
+                } else {
+                    number.style.border = "none";
+                    number_error.style.display = "block";
+                    number_error.innerText = data.family_name;
+                    location.replace("http://127.0.0.1:5500/invitation.html");
+                    return guest;
+                }
+            })  
+            .catch(err => console.error("Este"+ err));            
+            
+    } else {
+        number.style.border = "2px solid red";
+        number_error.style.display = "block";
+        number.focus();
+        number_error.innerText = "Ingresa un número válido"
+    }}
 
 
 
