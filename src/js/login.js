2f4cabdd03a56cdee95d505ea0f47fee527cb3b0
Sergio Tovar <stovarm@gmail.com>
2021-09-23 09:36:04 -0500

    Js. change color of response for blue(status=200), Style and html optimeze responsive

--- src/js/login.js
+++ src/js/login.js
@@ -1,3 +1,40 @@
+//CODIGO 2 CON BUTTON ONCLICK
+
+function loginValidate() {
+
+    const number = document.forms['form']['phone_number'];
+    const number_error = document.getElementById('phone_error');
+    const API = 'https://wedding-325203.rj.r.appspot.com/login/';
+    let guest = document.getElementById('phone').value;
+    let url = (`${API}${guest}`);
+    
+    if (guest.length == 10){
+        fetch(url)
+            .then(response => response.json())                  
+            .then(data => {
+                if (data.detail == 'User not found'){
+                   number.style.border = "2px solid red";
+                   number_error.style.display = "block";
+                   number.focus();
+                   number_error.innerText = "Usuario No existe"
+                } else {
+                    number.style.border = "none";
+                    number_error.style.display = "block";
+                    number_error.style.color ="blue";
+                    number_error.innerText = data.family_name;
+                    location.replace("https://wedding-apis-system.github.io/Frontend-Wedding/invitation.html");
+                    sessionStorage.setItem('numberId', guest);
+                }
+            })  
+            .catch(err => console.error("Este"+ err));            
+            
+    } else {
+        number.style.border = "2px solid red";
+        number_error.style.display = "block";
+        number.focus();
+        number_error.innerText = "Ingresa un número válido"
+    }}
+
 /* //CODIGO 1 CON SUBMIT
 
 const form = document.getElementById('login_form');
@@ -45,40 +82,3 @@ form.addEventListener('submit',(e) =>{
         console.log("Incorrecto");
     }
 })   */
-
-
-//CODIGO 2 CON BUTTON ONCLICK
-
-function loginValidate() {
-
-    const number = document.forms['form']['phone_number'];
-    const number_error = document.getElementById('phone_error');
-    const API = 'https://wedding-325203.rj.r.appspot.com/login/';
-    let guest = document.getElementById('phone').value;
-    let url = (`${API}${guest}`);
-    
-    if (guest.length == 10){
-        fetch(url)
-            .then(response => response.json())                  
-            .then(data => {
-                if (data.detail == 'User not found'){
-                   number.style.border = "2px solid red";
-                   number_error.style.display = "block";
-                   number.focus();
-                   number_error.innerText = "Usuario No existe"
-                } else {
-                    number.style.border = "none";
-                    number_error.style.display = "block";
-                    number_error.innerText = data.family_name;
-                    location.replace("https://wedding-apis-system.github.io/Frontend-Wedding/invitation.html");
-                    sessionStorage.setItem('numberId', guest);
-                }
-            })  
-            .catch(err => console.error("Este"+ err));            
-            
-    } else {
-        number.style.border = "2px solid red";
-        number_error.style.display = "block";
-        number.focus();
-        number_error.innerText = "Ingresa un número válido"
-    }}
